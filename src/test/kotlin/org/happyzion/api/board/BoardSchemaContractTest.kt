a58db8f3166fc4b6dfa1aa8760c032b23c5462ee
Chohanwool <hanwoolc95@gmail.com>
2026-05-15 20:30:48 +0900

    refactor: flyway 마이그레이션 반영

--- src/test/kotlin/org/happyzion/api/board/BoardSchemaContractTest.kt
+++ src/test/kotlin/org/happyzion/api/board/BoardSchemaContractTest.kt
@@ -22,6 +22,8 @@ class BoardSchemaContractTest {
             "V1__create_happyzion_schema.sql",
             "V2__create_site_setting.sql",
             "V3__allow_main_video_upload_token.sql",
+            "V4__create_mission_history.sql",
+            "V5__seed_mission_history.sql",
         )
     }
 
